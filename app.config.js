73387bf2efed4507c299db1227dd1f9b812f4e91
Raman <ramanmcreates@gmail.com>
2026-04-22 16:09:09 -0400

    chore: bump build number to 34

--- app.config.js
+++ app.config.js
@@ -64,7 +64,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: IS_PROD ? "com.airraman.deepwork" : "com.airraman.deepwork.dev",
-      buildNumber: "33",
+      buildNumber: "34",
       googleServicesFile: "./ios/DeepWorkioDev/GoogleService-Info.plist",
       infoPlist: {
         UIBackgroundModes: [
